a630b38eab6f9c32053f214e99fdcdf5a54a465c
LexYu <84486286+TLexYuW@users.noreply.github.com>
2022-11-13 22:30:17 +0800

    First Commit

---
+++ src/test/java/com/lexyu/springboot/SpringbootDockerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lexyu.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
